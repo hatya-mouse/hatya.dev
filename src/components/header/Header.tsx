82532d1eb464d0abfd43ea58e59b2c0b3c179a7b
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-02-13 17:44:06 +0900

    Add header bottom border and center content

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,11 +3,13 @@ import Avatar from "../avatar/avatar";
 
 export default function Header() {
     return (
-        <header className="fixed backdrop-blur-lg top-0 left-0 right-0 flex flex-row h-12 px-4 justify-center header-color">
-            <Link className="flex flex-row gap-3 items-center" href="/">
-                <Avatar width={28} height={28} />
-                <h4>Hatya</h4>
-            </Link>
+        <header className="fixed backdrop-blur-lg top-0 left-0 right-0 h-12 px-4 header-color">
+            <div className="flex flex-col justify-center items-center h-full w-full">
+                <Link className="flex flex-row gap-3 items-center" href="/">
+                    <Avatar width={28} height={28} />
+                    <h4>Hatya</h4>
+                </Link>
+            </div>
         </header>
     );
 }
